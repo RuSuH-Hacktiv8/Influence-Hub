57b9538cbc59e6bf78dbcf98450f7fb2d7b40e03
msayyids <muhammadsayyid525@gmail.com>
2023-10-11 20:11:20 +0700

    add notification email

---
+++ Notification/email_notification.go
@@ -0,0 +1,42 @@
+package notification
+
+import (
+	"os"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
+
+func SuccessRegisterEmail(user *models.User) error {
+	from := mail.NewEmail("Influencer Hub", "mhandyalfurqon@gmail.com")
+	subject := "Konfirmasi Pendaftaran"
+	to := mail.NewEmail(user.Username, user.Email)
+	plainTextContent := "Terima kasih telah mendaftar!"
+	htmlContent, err := os.ReadFile("success_register.html")
+	if err != nil {
+		return err
+	}
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, string(htmlContent))
+
+	client := sendgrid.NewSendClient("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") // Ganti dengan API key SendGrid kamu
+	_, err = client.Send(message)
+
+	return err
+}
+
+func SuccessApplyCampaign(user *models.User) error {
+	from := mail.NewEmail("Influencer Hub", "mhandyalfurqon@gmail.com")
+	subject := "Konfirmasi apply Campaign"
+	to := mail.NewEmail(user.Username, user.Email)
+	plainTextContent := "Terima kasih telah mendaftar!"
+	htmlContent, err := os.ReadFile("success_apply_campaign.html")
+	if err != nil {
+		return err
+	}
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, string(htmlContent))
+
+	client := sendgrid.NewSendClient("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")
+	_, err = client.Send(message)
+
+	return err
+}
--- Notification/go.mod
+++ Notification/go.mod
@@ -1,3 +1,11 @@
 module influence-hub-notification
 
 go 1.21.0
+
+require github.com/sendgrid/sendgrid-go v3.13.0+incompatible
+
+require (
+	github.com/sendgrid/rest v2.6.9+incompatible // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/net v0.17.0 // indirect
+)
