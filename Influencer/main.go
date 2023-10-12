ac3291b94787f40c0cfcd268ab8bf3319dc1dbe8
Rathief <rafie.fadlurahman@gmail.com>
2023-10-13 01:05:30 +0700

    added daily flwr count update

--- Influencer/main.go
+++ Influencer/main.go
@@ -4,7 +4,9 @@ import (
 	"influence-hub-influencer/config"
 	"influence-hub-influencer/controller"
 	"influence-hub-influencer/repository"
+	"time"
 
+	"github.com/go-co-op/gocron"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/labstack/echo/v4"
 )
@@ -13,6 +15,10 @@ func main() {
 	db := config.ConnectDb()
 	repository := repository.NewRepository(db)
 	controller := controller.NewController(repository)
+
+	s := gocron.NewScheduler(time.UTC)
+	s.Every(1).Day().At("00:00").Do(controller.UpdateFollowerCount)
+	s.StartAsync()
 
 	e := echo.New()
 	e.POST("/register", controller.Register)
