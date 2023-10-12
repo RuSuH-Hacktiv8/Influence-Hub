73a59cab4f2596747933adf7eca10a58f962c49c
Rathief <rafie.fadlurahman@gmail.com>
2023-10-12 11:03:45 +0700

    register and apply campaign notif on

--- Notification/handler/handler.go
+++ Notification/handler/handler.go
@@ -1,15 +1,51 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import (
+	"influence-hub-notification/models"
+	"influence-hub-notification/notification"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func SuccessRegister(c echo.Context) error {
+	var user models.User
+	if err := c.Bind(&user); err != nil {
+		c.JSON(http.StatusBadRequest, map[string]any{
+			"error":   err.Error(),
+			"details": "Failed on binding json input",
+		})
+		return err
+	}
 
-	return nil
+	if err := notification.SuccessRegisterEmail(&user); err != nil {
+		c.JSON(http.StatusInternalServerError, map[string]any{
+			"error":   err.Error(),
+			"details": "Failed on sending email",
+		})
+		return err
+	}
+	return c.JSON(http.StatusOK, "email sent")
 }
 
 func SuccessApplyCampaign(c echo.Context) error {
+	var user models.User
+	if err := c.Bind(&user); err != nil {
+		c.JSON(http.StatusBadRequest, map[string]any{
+			"error":   err.Error(),
+			"details": "Failed on binding json input",
+		})
+		return err
+	}
 
-	return nil
+	if err := notification.SuccessApplyCampaign(&user); err != nil {
+		c.JSON(http.StatusInternalServerError, map[string]any{
+			"error":   err.Error(),
+			"details": "Failed on sending email",
+		})
+		return err
+	}
+	return c.JSON(http.StatusOK, "email sent")
 }
 
 func PaymentReceived(c echo.Context) error {
