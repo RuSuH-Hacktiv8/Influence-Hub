7f609a6a987cbc683f30c061751ea856d13876fd
Rathief <rafie.fadlurahman@gmail.com>
2023-10-12 10:51:03 +0700

    email server coded

---
+++ Notification/handler/handler.go
@@ -0,0 +1,18 @@
+package handler
+
+import "github.com/labstack/echo/v4"
+
+func SuccessRegister(c echo.Context) error {
+
+	return nil
+}
+
+func SuccessApplyCampaign(c echo.Context) error {
+
+	return nil
+}
+
+func PaymentReceived(c echo.Context) error {
+
+	return nil
+}
